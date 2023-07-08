16fd2749c092c6265baa665b8d4a04bb37c41422
Brian Thomas <brianjamesthomas@live.com>
2023-07-08 12:12:53 +0100

    Set up the initial project structure

---
+++ src/test/java/io/learnk8s/knotejava/KnotejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.learnk8s.knotejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnotejavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
